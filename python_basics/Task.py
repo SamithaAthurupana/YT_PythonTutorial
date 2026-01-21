266843e5472468c3a6952019884ad4bf0ac5b8ff
SamithaAthurupana <athurupana1998@gmail.com>
2026-01-22 03:39:26 +0530

    first day projects

---
+++ python_basics/Task.py
@@ -0,0 +1,19 @@
+# Scenario:
+# Student Registration System
+#
+# A student registers for a course
+# We want to:
+#     Display student details
+#     Identify data types
+#     Use print()
+#     Use f-strings
+#     Use string concatenation
+
+
+# Program Explanation
+# We will store:
+# Student name (string), Age (integer), Course name (string), Is student active (boolean)
+#
+# Then:
+# Print details, Check data types, Combine text and values
+
---
+++ python_basics/main.py
@@ -0,0 +1,5 @@
+# Simple print()
+# Data Types in Python
+# Identify Type of Data – type()
+# f"" String (Formatted String)
+# String Concatenation
